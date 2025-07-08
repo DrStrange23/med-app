57bfe99ebc880a09b6f66b7bf35c129e32b31535
DrStrange23 <carloszuniga148@gmail.com>
2025-07-08 17:45:24 -0400

    Creating component React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+import Landing_Page from './components/Landing_Page/Landing_Page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Configurar BrowserRouter para enrutamiento */}
+        <BrowserRouter>
+          {/* Mostrar el componente Navbar */}
+          <Navbar/>
+          <Landing_Page />
+          {/* Configurar las Rutas para diferentes páginas */}
+          <Routes>
+            {/* Definir componentes Route individuales para diferentes páginas */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
